9f4be8369a58d66998f2663fb4c3e5cdc46d0753
Kirill-'kaswister <yslfm0@gmai.com>
2023-02-12 22:04:17 +0300

    feat: add stylint with fix errors and create media 1000 navigation, button for navigation

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+//carousel
+
 const owl = $('.owl-carousel');
 
 owl.owlCarousel({
@@ -16,3 +18,13 @@ $('.slider__btn__next').click(function() {
     owl.trigger('next.owl.carousel');
 })
 
+//nav icon
+
+const navButton = document.querySelector('.nav__toggle')
+const menuIcon = document.querySelector('.menu-icon')
+const nav = document.querySelector('.nav')
+
+navButton.onclick = function(){
+  nav.classList.toggle('nav--mobile')
+  menuIcon.classList.toggle('menu-icon-active');
+}
